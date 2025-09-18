dd275ad8d914e1161453a9f489d8d3448b7ae684
bbriannab <bribribb.bb@gmail.com>
2025-09-17 22:06:07 -0500

    seed_dump

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_09_02_193700) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_02_193700) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Book.create!([
+    { title: 'Harry Potter' },
+    { title: 'The Hunger Games' },
+    { title: 'The Lightning Thief' },
+    { title: 'Twilight' },
+    { title: 'The Mortal Instruments' }
+])
